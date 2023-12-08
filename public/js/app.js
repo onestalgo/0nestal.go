921f61b40046575c2ad7348d3e7deb1b63998a98
onestalgo <149548486+onestalgo@users.noreply.github.com>
2023-12-08 07:12:27 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setupCanvasEventListeners();
     setupSocketListeners();
+    enterFullScreenMode();
 
     function setupCanvasEventListeners() {
         canvas.addEventListener('click', function (e) {
