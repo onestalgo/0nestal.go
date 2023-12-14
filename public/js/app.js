af43282c95c730f7f3ec343fb8021b4059455917
onestalgo <149548486+onestalgo@users.noreply.github.com>
2023-12-14 01:47:47 +0100

    less lags?

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
+
     const uploadButton = document.getElementById('uploadButton');
     const fileInput = document.getElementById('fileInput');
 
@@ -22,8 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas.width = 1440;  // Set desired dimensions
     canvas.height = 900;
 
-    setupCanvasEventListeners();
-    setupSocketListeners();
+  
     
 
     function setupCanvasEventListeners() {
@@ -31,13 +31,27 @@ document.addEventListener('DOMContentLoaded', function () {
             const rect = canvas.getBoundingClientRect();
             const clickX = e.clientX - rect.left + window.scrollX;
             const clickY = e.clientY - rect.top + window.scrollY;
-    
-            images.forEach(image => {
-                if (clickX > image.x && clickX < image.x + image.width && clickY > image.y && clickY < image.y + image.height && !image.isAnimating) {
-                    handleImageResizeAndAnimation(image, clickX, clickY);
-                }
-            });
+
+            const clickedImage = getTopmostImageAtClick(clickX, clickY);
+            if (clickedImage) {
+                handleImageResizeAndAnimation(clickedImage, clickX, clickY);
+            }
         });
+        requestAnimationFrame(drawImages);
+    }
+
+
+    function getTopmostImageAtClick(x, y) {
+        for (let i = images.length - 1; i >= 0; i--) {
+            const img = images[i];
+            if (x >= img.x && x <= img.x + img.width && y >= img.y && y <= img.y + img.height) {
+                return img;
+            }
+        }
+        return null;
+    }
+    
+
     
         function handleImageResizeAndAnimation(image, clickX, clickY) {
                 if (clickX > image.x && clickX < image.x + image.width && clickY > image.y && clickY < image.y + image.height && !image.isAnimating) {
@@ -45,8 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     const leftThird = image.x + thirdWidth;
                     const rightThird = image.x + 2 * thirdWidth;
     
-                    const deltaX = clickX < leftThird ? -10 : clickX > rightThird ? 10 : 0;
-                    const increase = 10;
+                    const deltaX = clickX < leftThird ? -18 : clickX > rightThird ? 18 : 0;
+                    const increase = 15;
                     const newHeight = image.height + increase;
                     const newWidth = newHeight * (image.img.width / image.img.height);
                     const newCenterX = image.x + image.width / 2 + deltaX;
@@ -57,39 +71,53 @@ document.addEventListener('DOMContentLoaded', function () {
                                  
 
                     const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-                    animateBorder(image, newX, newY, newWidth, newHeight, randomColor);
-    
+                    drawStripedBorder(newX, newY, newWidth, newHeight, randomColor);
+                
                     setTimeout(() => {
                         image.x = newX;
                         image.y = newY;
                         image.width = newWidth;
                         image.height = newHeight;
-                        image.isAnimating = false;
+                        drawImages();
                         socket.emit('resizeAndMoveImage', { src: image.img.src, x: newX, y: newY, width: newWidth, height: newHeight });
-                        checkAndDeleteImage(image);
-                    }, 500);
+        
+        // Check and delete the image if it's off the canvas after resizing
+        checkAndDeleteImage(image);
+        }, 300);
                 }
-            }
-
-   
-
-        function checkAndDeleteImage(image) {
-            const oneThirdWidth = image.width / 3;
-            const oneThirdHeight = image.height / 3;
-            const offCanvas = image.x + oneThirdWidth < 0 || image.x + 2 * oneThirdWidth > canvas.width ||
-                              image.y + oneThirdHeight < 0 || image.y + 2 * oneThirdHeight > canvas.height;
-        
-            //const tooLarge = image.width >= 1000 || image.height >= 1000;
-        
-            if (offCanvas) {
-                socket.emit('deleteImage', { src: image.img.src });
                 
-                const index = images.indexOf(image);
-                if (index > -1) {
-                    images.splice(index, 1);
-                    drawImages(); // Redraw the canvas immediately
+                function drawStripedBorder(x, y, width, height) {
+                    const stripeWidth = 10;
+                    const stripeSpacing = 5;
+                    const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+                
+                    ctx.save();
+                    ctx.strokeStyle = randomColor;
+                    ctx.lineWidth = 3;
+                    ctx.setLineDash([stripeWidth, stripeSpacing]);
+                    ctx.strokeRect(x, y, width, height);
+                    ctx.restore();
                 }
-            }
+
+               
+    }
+   
+    function checkAndDeleteImage(image) {
+        const oneThirdWidth = image.width / 3;
+        const offCanvasLeft = image.x + oneThirdWidth < 0;
+        const offCanvasRight = image.x + 2 * oneThirdWidth > canvas.width;
+    
+        if (offCanvasLeft || offCanvasRight) {
+            socket.emit('deleteImage', { src: image.img.src });
+            removeImageFromCanvas(image);
+        }
+    }
+    
+    function removeImageFromCanvas(image) {
+        const index = images.indexOf(image);
+        if (index > -1) {
+            images.splice(index, 1);
+            drawImages(); // Redraw the canvas to reflect the deletion
         }
     }
 
@@ -97,105 +125,34 @@ document.addEventListener('DOMContentLoaded', function () {
         socket.on('initialImages', (initialImages) => {
             images = initialImages.map(imageData => {
                 const img = new Image();
-                img.onload = () => drawImages(); // Draw images as they load
+                img.onload = () => drawImages();
                 img.src = imageData.src;
-                return {...imageData, img};
+                return { ...imageData, img };
             });
         });
-    
+
         socket.on('imageUpdate', data => {
             updateOrAddImage(data);
             drawImages();
         });
-  
-        socket.on('resizeAnimationStart', (data) => {
-            const imageToAnimate = images.find(img => img.src === data.src);
-            if (imageToAnimate) {
-                const newWidth = data.width;
-                const newHeight = data.height;
-                const newX = data.x;
-                const newY = data.y;
-                const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.5)`;
-        
-                // Start animation on all clients
-        
-                setTimeout(() => {
-                    imageToAnimate.x = newX;
-                    imageToAnimate.y = newY;
-                    imageToAnimate.width = newWidth;
-                    imageToAnimate.height = newHeight;
-                    imageToAnimate.isAnimating = false;
-                    drawImages();
-                }, 500); // Ensure this duration matches the animation duration
-           
-                animateBorder(imageToAnimate, newX, newY, newWidth, newHeight, randomColor);
 
-            }
-
-        });
-    
         socket.on('imageDeleted', data => {
             const indexToDelete = images.findIndex(image => image.src === data.src);
             if (indexToDelete !== -1) {
-                images.splice(indexToDelete, 1); // Remove the image from the array
-                drawImages(); // Redraw the canvas to reflect the deletion
+                images.splice(indexToDelete, 1);
+                drawImages();
             }
         });
     }
 
-    function animateBorder(image, x, y, width, height, color) {
-        let opacity = 0;
-        const startTime = Date.now();
-        const duration = 500;
-    
-        function animate() {
-            const currentTime = Date.now();
-            const elapsed = currentTime - startTime;
-            opacity = Math.min(elapsed / duration, 1); // Ensure opacity doesn't exceed 1
-    
-            // Clear the canvas
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-            // Draw all images and the animated border in their original order
-            images.forEach(img => {
-                if (img !== image) {
-                    ctx.drawImage(img.img, img.x, img.y, img.width, img.height);
-                } else {
-                    // Draw animated border for the currently animating image
-                    ctx.save();
-                    ctx.globalAlpha = opacity;
-                    ctx.fillStyle = color;
-                    ctx.fillRect(x, y, width, height);
-                    ctx.restore();
-    
-                    // Draw the animating image itself
-                    ctx.drawImage(img.img, img.x, img.y, img.width, img.height);
-                }
-            });
-    
-            if (opacity < 1) {
-                requestAnimationFrame(animate);
-            } else {
-                // Redraw images to ensure correct ordering after animation completes
-                drawImages();
-            }
-        }
-    
-        animate();
-    }
-    
     function drawImages() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
         images.forEach(image => {
             if (!image.isAnimating && image.x > -10000 && image.y > -10000) {
                 ctx.drawImage(image.img, image.x, image.y, image.width, image.height);
             }
         });
     }
-
-
-    
 
 
     uploadButton.addEventListener('click', function () {
@@ -214,9 +171,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         compressAndUploadImage(file);
     });
-    
     function updateOrAddImage(data) {
-        const index = images.findIndex(image => image.src === data.src);
+        const index = images.findIndex(img => img.src === data.src);
         if (index !== -1) {
             images[index] = {...images[index], ...data};
         } else {
@@ -228,6 +184,8 @@ document.addEventListener('DOMContentLoaded', function () {
             img.src = data.src;
         }
     }
+
+
     function compressAndUploadImage(file) {
         compressImage(file, 800, 800, 0.7, (compressedBlob) => {
             let reader = new FileReader();
@@ -383,7 +341,8 @@ setInterval(() => {
     uploadCanvasImage(canvasImage);
 }, 26400000); // 5 hours in milliseconds (5 * 60 * 60 * 1000)
 
-   
+setupCanvasEventListeners();
+setupSocketListeners();
 
 });
 
