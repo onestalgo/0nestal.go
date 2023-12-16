cec32fbae1189926f582ab8e3f560c938992630f
onestalgo <149548486+onestalgo@users.noreply.github.com>
2023-12-16 06:58:50 +0100

    toolbarissue

--- public/js/app.js
+++ public/js/app.js
@@ -8,25 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
    const socket = io(window.location.origin);
     let images = [];
     
-    
-    const toolbar = document.getElementById('toolbar'); // Replace with your toolbar's ID
-    const canvasContainer = document.getElementById('canvas-container');
-
-    function adjustCanvasHeight() {
-        const toolbarHeight = toolbar.offsetHeight;
-        canvasContainer.style.height = `calc(100vh - ${toolbarHeight}px)`;
-    }
-
-    // Check if the screen width is that of a mobile device
-    // You can adjust the max-width value to suit your needs
-    const mobileWidth = 768; // pixels
-
-    if (window.innerWidth <= mobileWidth) {
-        // Adjust height on load and on window resize, but only for mobile
-        adjustCanvasHeight();
-        window.addEventListener('resize', adjustCanvasHeight);
-    }
-
 
     // JavaScript code for toggling the text overlay
    // JavaScript code for toggling the text overlay
